2e480ec28d70514fbbd62e8f8a77a3639a64aaa5
Weiwei Wang <weiwei.wang@neuralgalaxy.com>
2022-06-07 15:57:47 +0800

    bold preprocess pipeline运行成功

--- deepprep_pipeline/bold_preprocess.py
+++ deepprep_pipeline/bold_preprocess.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 import json
 import shutil
@@ -11,6 +12,11 @@ import numpy as np
 import voxelmorph as vxm
 import tensorflow as tf
 import SimpleITK as sitk
+from app.surface_projection import surface_projection as sp
+from app.volume_projection import volume_projection as vp
+from app.utils.utils import timing_func
+from app.filters.filters import gauss_nifti, bandpass_nifti
+from app.regressors.regressors import compile_regressors, regression
 
 
 def set_envrion():
@@ -31,6 +37,7 @@ def set_envrion():
     os.environ['PATH'] = '/usr/local/fsl/bin:' + os.environ['PATH']
 
 
+@timing_func
 def vxm_registraion(atlas_type, model_file, workdir, norm_path, trf_path, warp_path, warped_path):
     vxm_warp_path = workdir / 'warp.nii.gz'
     vxm_warped_path = workdir / 'warped.nii.gz'
@@ -270,6 +277,7 @@ def vxm_warp_bold_2mm(resid_t1, affine_file, warp_file, warped_file, verbose=Fal
     return warped_bold_img
 
 
+@timing_func
 def warp_bold_2mm(subj_func_path, subj, workdir, norm_file, resid_file, reg_file, save_file, verbose=False):
     resid_t1_file = subj_func_path / f'{subj}_native_t1_2mm.nii.gz'
     # native_bold_to_T1_2mm(residual_file, subj, subj_t1_file, reg_file, resid_t1_file)
@@ -281,6 +289,246 @@ def warp_bold_2mm(subj_func_path, subj, workdir, norm_file, resid_file, reg_file
     warped_img = vxm_warp_bold_2mm(resid_t1, affine_file, warp_file, warped_file, verbose=verbose)
     # bold_smooth_6(warped_file, smoothed_file)
     bold_smooth_6_ants(warped_img, save_file, verbose=True)
+
+
+def dimstr2dimno(dimstr):
+    if 'x' in dimstr:
+        return 0
+
+    if 'y' in dimstr:
+        return 1
+
+    if 'z' in dimstr:
+        return 2
+
+
+def swapdim(infile, a, b, c, outfile):
+    '''
+    infile  - str. Path to file to read and swap dimensions of.
+    a       - str. New x dimension.
+    b       - str. New y dimension.
+    c       - str. New z dimension.
+    outfile - str. Path to file to create.
+
+    Returns None.
+    '''
+
+    # Read original file.
+    img = nib.load(infile)
+
+    # Build orientation matrix.
+    ornt = np.zeros((3, 2))
+    order_strs = [a, b, c]
+    dim_order = list(map(dimstr2dimno, order_strs))
+    i_dim = np.argsort(dim_order)
+    for i, dim in enumerate(i_dim):
+        ornt[i, 1] = -1 if '-' in order_strs[dim] else 1
+
+    ornt[:, 0] = i_dim
+
+    # Transform and save.
+    newimg = img.as_reoriented(ornt)
+    nib.save(newimg, outfile)
+
+
+@timing_func
+def bold_skip_reorient(preprocess_dir, subj):
+    runs = os.listdir(preprocess_dir / subj / 'bold')
+    for run in runs:
+        bold_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld{run}_rest.nii.gz'
+        skip_bold_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld{run}_rest_skip.nii.gz'
+        # skip 0 frame
+        sh.mri_convert('-i', bold_file, '-o', skip_bold_file, _out=sys.stdout)
+        # reorient
+        reorient_bold_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld{run}_rest_reorient_skip.nii.gz'
+        swapdim(str(skip_bold_file), 'x', '-y', 'z', str(reorient_bold_file))
+
+
+@timing_func
+def preprocess_common(preprocess_dir, subj, subj_func_path, workdir):
+    bold_skip_reorient(preprocess_dir, subj)
+    runs = os.listdir(preprocess_dir / subj / 'bold')
+    for run in runs:
+        src_bold_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld{run}_rest_reorient_skip.nii.gz'
+        dst_bold_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld_rest_reorient_skip.nii.gz'
+        os.rename(src_bold_file, dst_bold_file)
+
+    # stc
+    input_fname = f'{subj}_bld_rest_reorient_skip'
+    output_fname = f'{subj}_bld_rest_reorient_skip_faln'
+    shargs = [
+        '-s', subj,
+        '-d', preprocess_dir,
+        '-fsd', 'bold',
+        '-so', 'odd',
+        '-ngroups', 1,
+        '-i', input_fname,
+        '-o', output_fname,
+        '-nolog']
+    sh.stc_sess(*shargs, _out=sys.stdout)
+
+    # mk_template
+    shargs = [
+        '-s', subj,
+        '-d', preprocess_dir,
+        '-fsd', 'bold',
+        '-funcstem', f'{subj}_bld_rest_reorient_skip_faln',
+        '-nolog']
+    sh.mktemplate_sess(*shargs, _out=sys.stdout)
+
+    # mc
+    shargs = [
+        '-s', subj,
+        '-d', preprocess_dir,
+        '-per-session',
+        '-fsd', 'bold',
+        '-fstem', f'{subj}_bld_rest_reorient_skip_faln',
+        '-fmcstem', f'{subj}_bld_rest_reorient_skip_faln_mc',
+        '-nolog']
+    sh.mc_sess(*shargs, _out=sys.stdout)
+
+    # register
+    for run in runs:
+        mov_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld_rest_reorient_skip_faln_mc.nii.gz'
+        reg_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld_rest_reorient_skip_faln_mc.register.dat'
+        shargs = [
+            '--bold',
+            '--s', subj,
+            '--mov', mov_file,
+            '--reg', reg_file]
+        sh.bbregister(*shargs, _out=sys.stdout)
+
+    # mk_brainmask
+    recon_dir = Path(os.environ['SUBJECTS_DIR'])
+    for run in runs:
+        func_path = preprocess_dir / subj / 'bold' / run / f'{subj}.func.aseg.nii'
+        mov_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld_rest_reorient_skip_faln_mc.nii.gz'
+        reg_file = preprocess_dir / subj / 'bold' / run / f'{subj}_bld_rest_reorient_skip_faln_mc.register.dat'
+        shargs = [
+            '--seg', recon_dir / subj / 'mri/aparc+aseg.mgz',
+            '--temp', mov_file,
+            '--reg', reg_file,
+            '--o', func_path]
+        sh.mri_label2vol(*shargs, _out=sys.stdout)
+
+        wm_path = preprocess_dir / subj / 'bold' / run / f'{subj}.func.wm.nii.gz'
+        shargs = [
+            '--i', func_path,
+            '--wm',
+            '--erode', 1,
+            '--o', wm_path]
+        sh.mri_binarize(*shargs, _out=sys.stdout)
+
+        vent_path = preprocess_dir / subj / 'bold' / run / f'{subj}.func.ventricles.nii.gz'
+        shargs = [
+            '--i', func_path,
+            '--ventricles',
+            '--o', vent_path]
+        sh.mri_binarize(*shargs, _out=sys.stdout)
+
+        mask_path = preprocess_dir / subj / 'bold' / run / f'{subj}.brainmask.nii.gz'
+        shargs = [
+            '--reg', reg_file,
+            '--targ', recon_dir / subj / 'mri/brainmask.mgz',
+            '--mov', mov_file,
+            '--inv',
+            '--o', mask_path]
+        sh.mri_vol2vol(*shargs, _out=sys.stdout)
+
+        binmask_path = preprocess_dir / subj / 'bold' / run / f'{subj}.brainmask.bin.nii.gz'
+        shargs = [
+            '--i', mask_path,
+            '--o', binmask_path,
+            '--min', 0.0001]
+        sh.mri_binarize(*shargs, _out=sys.stdout)
+
+
+@timing_func
+def smooth_downsampling(preprocess_dir, bold_path, bldrun, subject):
+    subjects_dir = Path(os.environ['SUBJECTS_DIR'])
+    fsaverage6_dir = subjects_dir / 'fsaverage6'
+    if not fsaverage6_dir.exists():
+        src_fsaverage6_dir = Path(os.environ['FREESURFER_HOME']) / 'subjects' / 'fsaverage6'
+        os.symlink(src_fsaverage6_dir, fsaverage6_dir)
+
+    fsaverage5_dir = subjects_dir / 'fsaverage5'
+    if not fsaverage5_dir.exists():
+        src_fsaverage5_dir = Path(os.environ['FREESURFER_HOME']) / 'subjects' / 'fsaverage5'
+        os.symlink(src_fsaverage5_dir, fsaverage5_dir)
+
+    fsaverage4_dir = subjects_dir / 'fsaverage4'
+    if not fsaverage4_dir.exists():
+        src_fsaverage4_dir = Path(os.environ['FREESURFER_HOME']) / 'subjects' / 'fsaverage4'
+        os.symlink(src_fsaverage4_dir, fsaverage4_dir)
+
+    logs_path = preprocess_dir / subject / 'logs'
+    logs_path.mkdir(exist_ok=True)
+
+    surf_path = preprocess_dir / subject / 'surf'
+    surf_path.mkdir(exist_ok=True)
+
+    bldrun_path = bold_path / bldrun
+    reg_name = '%s_bld_rest_reorient_skip_faln_mc.register.dat' % (subject)
+    reg_path = bldrun_path / reg_name
+
+    resid_name = '%s_bld_rest_reorient_skip_faln' % (subject)
+    resid_name += '_mc_g1000000000_bpss_resid.nii.gz'
+    resid_path = bldrun_path / resid_name
+
+    for hemi in ['lh', 'rh']:
+        fs6_path = sp.indi_to_fs6(surf_path, subject, resid_path, reg_path, hemi)
+        sm6_path = sp.smooth_fs6(fs6_path, hemi)
+        sp.downsample_fs6_to_fs4(sm6_path, hemi)
+
+
+@timing_func
+def preprocess_rest(layout, preprocess_dir, subj, subj_func_path, workdir):
+    runs = os.listdir(preprocess_dir / subj / 'bold')
+    runs = [run for run in runs if os.path.isdir(preprocess_dir / subj / 'bold' / run)]
+
+    fcmri_dir = preprocess_dir / subj / 'fcmri'
+    fcmri_dir.mkdir(exist_ok=True)
+    bold_dir = preprocess_dir / subj / 'bold'
+    for run in runs:
+        # spatial_smooth
+        TR = layout.get_tr(subject=subj[4:], run=int(run))
+
+        mc_path = preprocess_dir / subj / 'bold' / run / f'{subj}_bld_rest_reorient_skip_faln_mc.nii.gz'
+        gauss_path = gauss_nifti(str(mc_path), 1000000000)
+
+        # bandpass_filtering
+        bpss_path = bandpass_nifti(gauss_path, TR)
+
+        # compile_regressors, regression
+        all_regressors_path = compile_regressors(preprocess_dir, bold_dir, run, subj, fcmri_dir, bpss_path)
+        regression(bpss_path, all_regressors_path)
+
+        # smooth_downsampling
+        smooth_downsampling(preprocess_dir, bold_dir, run, subj)
+
+
+@timing_func
+def preprocess(layout, subj, subj_func_path, workdir):
+    bids_bolds = layout.get(subject=subj, suffix='bold', extension='.nii.gz')
+    preprocess_dir = workdir
+    subj_bold_dir = preprocess_dir / f'sub-{subj}' / 'bold'
+    subj_bold_dir.mkdir(parents=True, exist_ok=True)
+    for bids_bold in bids_bolds:
+        run = f"{bids_bold.entities['run']:03}"
+        (subj_bold_dir / run).mkdir(exist_ok=True)
+        bold_file = bids_bold.path
+        shutil.copy(bold_file, subj_bold_dir / run / f'sub-{subj}_bld{run}_rest.nii.gz')
+    preprocess_common(preprocess_dir, f'sub-{subj}', subj_func_path, workdir)
+    preprocess_rest(layout, preprocess_dir, f'sub-{subj}', subj_func_path, workdir)
+    for bids_bold in bids_bolds:
+        run = f"{bids_bold.entities['run']:03}"
+        src_resid_file = subj_bold_dir / run / f'sub-{subj}_bld_rest_reorient_skip_faln_mc_g1000000000_bpss_resid.nii.gz'
+        dst_resid_file = subj_func_path / f'sub-{subj}_run-{run}_resid.nii.gz'
+        shutil.copy(src_resid_file, dst_resid_file)
+        src_reg_file = subj_bold_dir / run / f'sub-{subj}_bld_rest_reorient_skip_faln_mc.register.dat'
+        dst_reg_file = subj_func_path / f'sub-{subj}_run-{run}_bbregister.register.dat'
+        shutil.copy(src_reg_file, dst_reg_file)
+    shutil.copytree(preprocess_dir / f'sub-{subj}' / 'surf', subj_func_path / 'surf')
 
 
 if __name__ == '__main__':
@@ -307,9 +555,11 @@ if __name__ == '__main__':
     with open(dataset_description_file, 'w') as jf:
         json.dump(dataset_description, jf, indent=4)
 
-    freesurfer_subjects_path = Path(layout.derivatives['freesurfer'].root)
-    atlas_type = 'MNI152_T1_2mm'
     set_envrion()
+    freesurfer_subjects_path = Path(layout.derivatives['freesurfer'].root)
+    os.environ['SUBJECTS_DIR'] = str(freesurfer_subjects_path)
+
+    atlas_type = 'MNI152_T1_2mm'
     for subj in subjs:
         deepprep_subj_path = derivative_deepprep_path / f'sub-{subj}'
         deepprep_subj_path.mkdir(exist_ok=True)
@@ -318,7 +568,8 @@ if __name__ == '__main__':
 
         workdir = Path('workdir')
         workdir.mkdir(exist_ok=True)
-        # T1 warp
+
+        # T1 warp to MNI2mm
         model_file = Path(__file__).parent / 'model' / 'voxelmorph' / atlas_type / 'model.h5'
         norm_file = freesurfer_subjects_path / f'sub-{subj}' / 'mri' / 'norm.mgz'
         trf_file = subj_func_path / f'sub-{subj}_affine.mat'
@@ -326,10 +577,17 @@ if __name__ == '__main__':
         warped_file = subj_func_path / f'sub-{subj}_warped.nii.gz'
         vxm_registraion(atlas_type, model_file, workdir, norm_file, trf_file, warp_file, warped_file)
 
-        # bold warp
-        resid = 'NC_15_bld010_rest_reorient_skip_faln_mc_g1000000000_bpss_resid.nii.gz'
-        resid_file = subj_func_path / f'sub-{subj}_resid.nii.gz'
-        reg_file = subj_func_path / f'sub-{subj}_bbregister.register.dat'
-        save_file = subj_func_path / f'sub-{subj}_resid_MIN2mm_sm6.nii.gz'
-        warp_bold_2mm(subj_func_path, subj, workdir, norm_file, resid_file, reg_file, save_file, verbose=False)
-        pass
+        bids_bolds = layout.get(subject=subj, suffix='bold', extension='.nii.gz')
+        preprocess(layout, subj, subj_func_path, workdir)
+
+        for bids_bold in bids_bolds:
+            bold_file = bids_bold.path
+            run = f"{bids_bold.entities['run']:03}"
+
+            # bold warp
+            resid_file = subj_func_path / f'sub-{subj}_run-{run}_resid.nii.gz'
+            reg_file = subj_func_path / f'sub-{subj}_run-{run}_bbregister.register.dat'
+            save_file = subj_func_path / f'sub-{subj}_resid_MIN2mm_sm6.nii.gz'
+            warp_bold_2mm(subj_func_path, subj, workdir, norm_file, resid_file, reg_file, save_file, verbose=False)
+
+        # shutil.rmtree(workdir)
