75b5830fd53c2f1b6836adda6d772dbf9f030d09
Kid-sunzhenyu <sun25939789@gmail.com>
2022-09-07 16:19:55 +0800

    test

--- deepprep_pipeline/interface/fastsurfer_test.py
+++ deepprep_pipeline/interface/fastsurfer_test.py
@@ -14,7 +14,7 @@ def Segment_test():
     network_axial_path = weight_dir / "Axial_Weights_FastSurferCNN" / "ckpts" / "Epoch_30_training_state.pkl"
 
     segment_node = Node(Segment(), f'segment_node')
-    segment_node.inputs.python_interpret = '/home/anning/miniconda3/envs/3.8/bin/python3'
+    segment_node.inputs.python_interpret = '/home/pbfs18/anaconda3/envs/3.8/bin/python3'
     segment_node.inputs.in_file = '/mnt/ngshare/Data_Mirror/SDCFlows_test/MSC1/derivatives/deepprep/Recon/sub-MSC01_ses-struct01_run-01/mri/orig.mgz'
     segment_node.inputs.out_file = '/mnt/ngshare/Data_Mirror/SDCFlows_test/MSC1/derivatives/deepprep/Recon/sub-MSC01_ses-struct01_run-01/mri/aparc.DKTatlas+aseg.deep.mgz'
     segment_node.inputs.eval_py = fastsurfer_eval
@@ -22,7 +22,7 @@ def Segment_test():
     segment_node.inputs.network_coronal_path = network_coronal_path
     segment_node.inputs.network_axial_path = network_axial_path
 
-    segment_node.run()
+    # segment_node.run()
 
     segment_node.inputs.conformed_file = '/mnt/ngshare/Data_Mirror/SDCFlows_test/MSC1/derivatives/deepprep/Recon/sub-MSC01_ses-struct01_run-01/mri/conformed.mgz'
     segment_node.run()
