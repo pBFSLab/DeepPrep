d9afe49dd63e458f451ec5a170303ba62d07ae63
lincong <lincong8722@qq.com>
2022-09-08 15:02:26 +0800

    add SampleSegmentationToSurfave block

--- deepprep_pipeline/interface/fastsurfer.py
+++ deepprep_pipeline/interface/fastsurfer.py
@@ -254,3 +254,62 @@ class UpdateAseg(BaseInterface):
         outputs["aseg_auto_file"] = self.inputs.aseg_auto_file
         outputs["cc_up_file"] = self.inputs.cc_up_file
         outputs["aparc_aseg_file"] = self.inputs.aparc_aseg_file
+
+
+class SampleSegmentationToSurfaveInputSpec(BaseInterfaceInputSpec):
+    subject_dir = Directory(exists=True, desc="subject dir", mandatory=True)
+    subject_id = Str(desc="subject id", mandatory=True)
+    python_interpret = File(exists=True, desc="python interpret", mandatory=True)
+    freesufer_home = Directory(exists=True, desc="freesufer_home", mandatory=True)
+    hemi = Str(desc="lh/rh", mandatory=True)
+    hemi_DKTatlaslookup_file = File(exists=True, desc="FastSurfer/recon_surf/{hemi}.DKTatlaslookup.txt", mandatory=True)
+    aparc_aseg_file = File(exists=True, desc="mri/aparc.DKTatlas+aseg.deep.withCC.mgz", mandatory=True)
+    smooth_aparc_file = File(exists=True, desc="Fastsurfer/recon_surf/smooth_aparc.py", mandatory=True)
+    hemi_white_preaparc_file = File(exists=True, desc="surf/{hemi}.white.preaparc", mandatory=True)
+    hemi_cortex_label_file = File(exists=True, desc="label/{hemi}.cortex.label", mandatory=True)
+
+    hemi_aparc_DKTatlas_mapped_prefix_file = File(exists=False, desc="label/{hemi}.aparc.DKTatlas.mapped.prefix.annot",
+                                                  mandatory=True)
+    hemi_aparc_DKTatlas_mapped_file = File(exists=False, desc="label/{hemi}.aparc.DKTatlas.mapped.annot",
+                                           mandatory=True)
+
+
+class SampleSegmentationToSurfaveOutputSpec(TraitedSpec):
+    hemi_aparc_DKTatlas_mapped_prefix_file = File(exists=False, desc="label/{hemi}.aparc.DKTatlas.mapped.prefix.annot",
+                                                  mandatory=True)
+    hemi_aparc_DKTatlas_mapped_file = File(exists=False, desc="label/{hemi}.aparc.DKTatlas.mapped.annot",
+                                           mandatory=True)
+
+
+class SampleSegmentationToSurfave(BaseInterface):
+    input_spec = SampleSegmentationToSurfaveInputSpec
+    output_spec = SampleSegmentationToSurfaveOutputSpec
+
+    time = 11 / 60  # 运行时间：分钟 / 单脑测试时间
+    cpu = 3.5  # 最大cpu占用：个
+    gpu = 0  # 最大gpu占用：MB
+
+    def _run_interface(self, runtime):
+        # sample input segmentation (aparc.DKTatlas+aseg orig) onto wm surface:
+        # map input aparc to surface (requires thickness (and thus pail) to compute projfrac 0.5),
+        # here we do projmm which allows us to compute based only on white
+        # this is dangerous, as some cortices could be < 0.6 mm, but then there is no volume label probably anyway.
+        # Also note that currently we cannot mask non-cortex regions here, should be done in mris_anatomical stats later
+        # the smoothing helps
+        cmd = f"mris_sample_parc -ct {self.inputs.freesufer_home}/average/colortable_desikan_killiany.txt " \
+              f"-file {self.inputs.hemi_DKTatlaslookup_file} -projmm 0.6 -f 5  " \
+              f"-surf white.preaparc {self.inputs.subject_id} {self.inputs.hemi} " \
+              f"aparc.DKTatlas+aseg.orig.mgz aparc.DKTatlas.mapped.prefix.annot"
+        run_cmd_with_timing(cmd)
+
+        cmd = f"{self.inputs.python_interpret} {self.inputs.smooth_aparc_file} " \
+              f"--insurf {self.inputs.hemi_white_preaparc_file} " \
+              f"--inaparc {self.inputs.hemi_aparc_DKTatlas_mapped_prefix_file} " \
+              f"--incort {self.inputs.hemi_cortex_label_file} " \
+              f"--outaparc {self.inputs.hemi_aparc_DKTatlas_mapped_file}"
+        run_cmd_with_timing(cmd)
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        outputs["hemi_aparc_DKTatlas_mapped_prefix_file"] = self.inputs.hemi_aparc_DKTatlas_mapped_prefix_file
+        outputs["hemi_aparc_DKTatlas_mapped_file"] = self.inputs.hemi_aparc_DKTatlas_mapped_file
