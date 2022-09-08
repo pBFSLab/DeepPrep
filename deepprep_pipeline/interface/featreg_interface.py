78c22c834a943786c244c6f6b0772350a8a3bf6f
NingAnMe <ninganme@qq.com>
2022-09-08 16:53:52 +0800

    change subject_dir to subjects_dir

--- deepprep_pipeline/interface/featreg_interface.py
+++ deepprep_pipeline/interface/featreg_interface.py
@@ -7,7 +7,7 @@ class FeatRegInputSpec(BaseInterfaceInputSpec):
     python_interpret = File(exists=True, mandatory=True, desc='the python interpret to use')
     featreg_py = File(exists=True, mandatory=True, desc="FeatReg script")
 
-    subject_dir = Directory(exists=True, desc='subject dir path', mandatory=True)
+    subjects_dir = Directory(exists=True, desc='subject dir path', mandatory=True)
     subject_id = Str(desc='subject id', mandatory=True)
     freesurfer_home = Directory(exists=True, desc='FreeSurfer HOME path', mandatory=True)
     hemisphere = Str(desc='hemisphere: lh or rh', mandatory=True)
@@ -29,15 +29,15 @@ class FeatReg(BaseInterface):
     gpu = 3500  # 最大gpu占用：MB
 
     def _run_interface(self, runtime):
-        subject_dir = self.inputs.subject_dir
+        subjects_dir = self.inputs.subjects_dir
         subject_id = self.inputs.subject_id
-        cmd = f'{self.inputs.python_interpret} {self.inputs.featreg_py} --sd {subject_dir} --sid {subject_id} ' \
+        cmd = f'{self.inputs.python_interpret} {self.inputs.featreg_py} --sd {subjects_dir} --sid {subject_id} ' \
               f'--fsd {self.inputs.freesurfer_home} --hemi {self.inputs.hemisphere}'
         run_cmd_with_timing(cmd)
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        subject_dir = Path(self.inputs.subject_dir) / self.inputs.subject_id
-        outputs['sphere_reg_file'] = subject_dir / 'surf' / f'{self.inputs.hemisphere}.sphere.reg'
+        subjects_dir = Path(self.inputs.subjects_dir) / self.inputs.subject_id
+        outputs['sphere_reg_file'] = subjects_dir / 'surf' / f'{self.inputs.hemisphere}.sphere.reg'
         return outputs
