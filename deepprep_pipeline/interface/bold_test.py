1be0d306a742c6c95ad8fcfff1096a37a0d30a14
NingAnMe <ninganme@qq.com>
2022-09-23 10:45:03 +0800

    structure workflow done

--- deepprep_pipeline/interface/bold_test.py
+++ deepprep_pipeline/interface/bold_test.py
@@ -3,37 +3,7 @@ from bold import BoldSkipReorient, MotionCorrection, Stc, Register, MkBrainmask,
 from pathlib import Path
 from nipype import Node
 import os
-import ants
-import bids
-import pandas as pd
-import numpy as np
-import csv
-import sh
-import nibabel as nib
-from sklearn.decomposition import PCA
-
-
-def set_envrion(threads: int = 1):
-    # FreeSurfer recon-all env
-    os.environ['FREESURFER_HOME'] = '/usr/local/freesurfer'
-    os.environ['FREESURFER'] = '/usr/local/freesurfer'
-    os.environ['SUBJECTS_DIR'] = '/usr/local/freesurfer/subjects'
-    os.environ['PATH'] = '/usr/local/freesurfer/bin:/usr/local/freesurfer/mni/bin:/usr/local/freesurfer/tktools:' + \
-                         '/usr/local/freesurfer/fsfast/bin:' + os.environ['PATH']
-    os.environ['MINC_BIN_DIR'] = '/usr/local/freesurfer/mni/bin'
-    os.environ['MINC_LIB_DIR'] = '/usr/local/freesurfer/mni/lib'
-    os.environ['PERL5LIB'] = '/usr/local/freesurfer/mni/share/perl5'
-    os.environ['MNI_PERL5LIB'] = '/usr/local/freesurfer/mni/share/perl5'
-    # FreeSurfer fsfast env
-    os.environ['FSF_OUTPUT_FORMAT'] = 'nii.gz'
-    os.environ['FSLOUTPUTTYPE'] = 'NIFTI_GZ'
-
-    # FSL
-    os.environ['PATH'] = '/usr/local/fsl/bin:' + os.environ['PATH']
-
-    # set threads
-    os.environ['OMP_NUM_THREADS'] = str(threads)
-    os.environ['ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS'] = str(threads)
+from run import set_envrion
 
 
 def BoldSkipReorient_test():
