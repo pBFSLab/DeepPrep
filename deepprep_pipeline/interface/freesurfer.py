449949de9f2136a6ef5754ab067bbff36561dfd9
youjia <ireneyou33@gmail.com>
2022-09-27 16:00:21 +0800

    freesurfer

--- deepprep_pipeline/interface/freesurfer.py
+++ deepprep_pipeline/interface/freesurfer.py
@@ -1204,11 +1204,11 @@ class BalabelsMult(BaseInterface):
         if not fsaverage_dir.exists():
             os.system(f"cp {Path(self.inputs.freesurfer_dir) / 'fsaverage'} {fsaverage_dir}")
         if not fsaverage4_dir.exists():
-            os.system(f"cp {Path(self.inputs.fsaverage4_dir) / 'fsaverage4'} {fsaverage4_dir}")
+            os.system(f"cp {Path(self.inputs.fsaverage_dir) / 'fsaverage4'} {fsaverage4_dir}")
         if not fsaverage5_dir.exists():
-            os.system(f"cp {Path(self.inputs.fsaverage5_dir) / 'fsaverage5'} {fsaverage5_dir}")
+            os.system(f"cp {Path(self.inputs.fsaverage_dir) / 'fsaverage5'} {fsaverage5_dir}")
         if not fsaverage6_dir.exists():
-            os.system(f"cp {Path(self.inputs.fsaverage6_dir) / 'fsaverage6'} {fsaverage6_dir}")
+            os.system(f"cp {Path(self.inputs.fsaverage_dir) / 'fsaverage6'} {fsaverage6_dir}")
 
 
         multipool(self.cmd, Multi_Num=2)
