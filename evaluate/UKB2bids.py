c125093d779992f7425694fb44cb00dc660ad059
Kid-sunzhenyu <sun25939789@gmail.com>
2022-10-03 13:48:43 +0800

    UKB2bids

--- evaluate/UKB2bids.py
+++ evaluate/UKB2bids.py
@@ -2,6 +2,7 @@ import os.path
 import shutil
 from pathlib import Path
 import zipfile
+import pandas as pd
 
 
 def unzip(zip_file, outpath):
@@ -13,61 +14,75 @@ def unzip(zip_file, outpath):
 
 
 if __name__ == '__main__':
-    UKB_dir = Path('/mnt/ngshare/Data_Orig/UKB')
-    T1_dir = UKB_dir / 'T1'
-    rest_dir = UKB_dir / 'rfMRI'
-    tmp_dir = UKB_dir / 'tmp'
-    bids_outpath = UKB_dir / 'BIDS'
-    sublist_txt = '/mnt/ngshare/Data_Orig/UKB/sublist.txt'
+    UKB_dir = '/mnt/ngshare2'
+    T1_dir = '/media/pbfs19/c97fb2f7-1015-4387-bd54-b6ff6a54b856/UKB/T1'
+    rest_dir = '/media/pbfs19/c97fb2f7-1015-4387-bd54-b6ff6a54b856/UKB/rfMRI'
+    tmp_dir = os.path.join(UKB_dir, 'tmp')
+    bids_outpath = os.path.join(UKB_dir, 'BIDS')
+    sublist_csv = '/home/pbfs19/workspace/f.20191.filtered_T1_01.csv'
 
-    # subjects_list = ['1000037']
-    subjects_list = []
-    with open(sublist_txt) as f:
-        for line in f:
-            line = line.split('_')[0]
-            subjects_list.append(line.strip())
+    df = pd.read_csv(sublist_csv)
+    df_data = pd.DataFrame(df)
+    subjects_list = list(df_data['sub'])
 
-    tmp_dir.mkdir(exist_ok=True)
-    bids_outpath.mkdir(exist_ok=True)
+    if os.path.exists(tmp_dir) is False:
+        os.makedirs(tmp_dir)
+    if os.path.exists(bids_outpath) is False:
+        os.makedirs(bids_outpath)
 
     for sub in subjects_list:
-        sub_tmp_path = tmp_dir / sub
-
+        sub = str(sub)
+        sub_tmp_path = os.path.join(tmp_dir, sub)
         if os.path.exists(sub_tmp_path) is True:
             print(sub + "： 已解压!")
             continue
+        else:
+            os.makedirs(sub_tmp_path)
 
-        sub_tmp_path.mkdir(exist_ok=True, parents=True)
+        sub_T1_zip = os.path.join(T1_dir,  f'{sub}_20252_2_0.zip')
+        sub_rest_zip = os.path.join(rest_dir, f'{sub}_20227_2_0.zip')
 
-        sub_T1_zip = T1_dir / f'{sub}_20252_2_0.zip'
-        sub_rest_zip = rest_dir / f'{sub}_20227_2_0.zip'
+        if os.path.exists(sub_T1_zip) is False or os.path.exists(sub_rest_zip) is False:
+            print(sub + '没有！！！！！！！！！！！！！')
+            continue
 
-        unzip(str(sub_T1_zip), sub_tmp_path)
-        unzip(str(sub_rest_zip), sub_tmp_path)
+        unzip(sub_T1_zip, sub_tmp_path)
+        unzip(sub_rest_zip, sub_tmp_path)
 
         print(sub + "： 已解压完成！")
 
     for sub in subjects_list:
-        sub_tmp_T1_file = tmp_dir / sub / 'T1' / 'T1.nii.gz'
-        sub_tmp_rest_file = tmp_dir / sub / 'fMRI' / 'rfMRI.nii.gz'
+        sub = str(sub)
+        sub_tmp_T1_file = os.path.join(tmp_dir, sub, 'T1', 'T1.nii.gz')
+        sub_tmp_rest_file = os.path.join(tmp_dir, sub, 'fMRI', 'rfMRI.nii.gz')
 
-        if os.path.exists(str(sub_tmp_T1_file)) is False:
-            sub_tmp_T1_file = tmp_dir / sub / 'T1' / 'unusable' / 'T1_orig_defaced.nii.gz'
-            sub_tmp_rest_file = tmp_dir / sub / 'fMRI' / 'unusable' / 'rfMRI.nii.gz'
+        if os.path.exists(sub_tmp_T1_file) is False:
+            sub_tmp_T1_file = os.path.join(tmp_dir, sub, 'T1', 'unusable', 'T1_orig_defaced.nii.gz')
+            if os.path.exists(sub_tmp_T1_file) is False:
+                sub_tmp_T1_file = os.path.join(tmp_dir, sub, 'T1', 'T1_orig_defaced.nii.gz')
+        if os.path.exists(sub_tmp_rest_file) is False:
+            sub_tmp_rest_file = os.path.join(tmp_dir, sub, 'fMRI', 'unusable', 'rfMRI.nii.gz')
+
+        if os.path.exists(sub_tmp_rest_file) is False:
+            continue
+
 
         new_sub = f'sub-{sub}'
 
-        sub_bids_T1_anat = bids_outpath / new_sub / 'ses-02' / 'anat'
-        sub_bids_rest_bold = bids_outpath / new_sub / 'ses-02' / 'func'
+        sub_bids_T1_anat = os.path.join(bids_outpath, new_sub, 'ses-02', 'anat')
+        sub_bids_rest_bold = os.path.join(bids_outpath, new_sub, 'ses-02', 'func')
 
-        sub_bids_T1_anat.mkdir(exist_ok=True, parents=True)
-        sub_bids_rest_bold.mkdir(exist_ok=True, parents=True)
+        if os.path.exists(sub_bids_T1_anat) is False:
+            os.makedirs(sub_bids_T1_anat)
+        if os.path.exists(sub_bids_rest_bold) is False:
+            os.makedirs(sub_bids_rest_bold)
 
-        sub_bids_T1_path = sub_bids_T1_anat / f'{new_sub}_ses-02_T1w.nii.gz'
-        sub_bids_rest_path = sub_bids_rest_bold / f'{new_sub}_ses-02_task-rest_run-01_bold.nii.gz'
+        sub_bids_T1_path = os.path.join(sub_bids_T1_anat, f'{new_sub}_ses-02_T1w.nii.gz')
+        sub_bids_rest_path = os.path.join(sub_bids_rest_bold, f'{new_sub}_ses-02_task-rest_run-01_bold.nii.gz')
 
-        if os.path.exists(str(sub_tmp_T1_file)) is False:
+        if os.path.exists(sub_bids_T1_path) is False:
             shutil.move(sub_tmp_T1_file, sub_bids_T1_path)
+        if os.path.exists(sub_bids_rest_path) is False:
             shutil.move(sub_tmp_rest_file, sub_bids_rest_path)
 
         print(sub + "： 已转为bids！")
