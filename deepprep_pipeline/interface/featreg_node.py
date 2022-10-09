e2bcac2f675322b8f322bfb0f8374fee7f714c0f
youjia <ireneyou33@gmail.com>
2022-10-09 13:15:52 +0800

    node

--- deepprep_pipeline/interface/featreg_node.py
+++ deepprep_pipeline/interface/featreg_node.py
@@ -6,6 +6,7 @@ from interface.run import run_cmd_with_timing, multipool
 class FeatRegInputSpec(BaseInterfaceInputSpec):
     python_interpret = File(exists=True, mandatory=True, desc='the python interpret to use')
     featreg_py = File(exists=True, mandatory=True, desc="FeatReg script")
+    base_dir = Directory(exists=True, desc="workflow cached dir", mandatory=True)
 
     subjects_dir = Directory(exists=True, desc='subject dir path', mandatory=True)
     subject_id = Str(desc='subject id', mandatory=True)
