fd86433bbfda9651555c65394617b5881be288f4
NingAnMe <ninganme@qq.com>
2022-11-02 13:43:22 +0800

    增加同步1500数据到pbfs20的代码

--- evaluate/rsync/rsync_deepprep_1500_to_pbfs20.py
+++ evaluate/rsync/rsync_deepprep_1500_to_pbfs20.py
@@ -33,6 +33,16 @@ def copy_deepprep_1500_to_pbfs20():
         subject_filter_ids = [i.replace('sub-', '') for i in subject_filter_ids]
         subject_filter_ids = set(subject_filter_ids)
 
+    for subject_id in os.listdir(src_recon_dir):
+        if 'fsaverage' in subject_id:
+            continue
+        if subject_filter_ids is not None and subject_id.split('-')[1] not in subject_filter_ids:
+            continue
+        else:
+            src_dir = src_recon_dir / subject_id
+            dst_dir = dst_recon_dir / subject_id
+            rsync(src_dir, dst_dir, user, host)
+
     for subject_id in os.listdir(src_bold_dir):
         if subject_filter_ids is not None and subject_id.split('-')[1] not in subject_filter_ids:
             continue
@@ -40,16 +50,6 @@ def copy_deepprep_1500_to_pbfs20():
             print(subject_id)
             src_dir = src_bold_dir / subject_id
             dst_dir = dst_bold_dir / subject_id
-            rsync(src_dir, dst_dir, user, host)
-
-    for subject_id in os.listdir(src_recon_dir):
-        if subject_id not in subject_filter_ids:
-            continue
-        elif 'fsaverage' in subject_id:
-            continue
-        else:
-            src_dir = src_recon_dir / subject_id
-            dst_dir = dst_recon_dir / subject_id
             rsync(src_dir, dst_dir, user, host)
 
 
