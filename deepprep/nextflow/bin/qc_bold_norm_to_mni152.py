f748a7c875893eee25a88562ba2c4da9b650b38e
Kid-sunzhenyu <sun25939789@gmail.com>
2023-09-05 10:27:36 +0800

    NEW 增加了norm投到MNI152的qc画图

--- deepprep/nextflow/bin/qc_bold_norm_to_mni152.py
+++ deepprep/nextflow/bin/qc_bold_norm_to_mni152.py
@@ -1,7 +1,6 @@
 import argparse
 import shutil
 
-import nibabel as nib
 import os
 from pathlib import Path
 import base64
@@ -16,50 +15,32 @@ svg_img_style = '''<style type="text/css">
 </style>
 '''
 
-svg_img_body_1 = '''    <g class="background-svg">
+svg_img_body_2 = '''    <g class="background-svg">
       <g transform="translate(0, 0) scale(1.0 1.0) ">
-      <image xlink:href="{back_encode}" />
+      <image xlink:href="{back_encode}" />      
+       <text x="10" y="40" font-family="Arial" font-size="50" fill="white">moved</text>
+      </g>
+    </g>
+    <g class="foreground-svg">
+      <g transform="translate(0, 0) scale(1.0 1.0) ">
+      <image xlink:href="{fore_encode}" />
+       <text x="10" y="40" font-family="Arial" font-size="50" fill="white">fixed</text>
       </g>
     </g>
 '''
-
 svg_img_tail = '</svg>'
 
 
-def set_environ(freesurfer_home):
-    # FreeSurfer
-    os.environ['FREESURFER_HOME'] = freesurfer_home
-    os.environ['SUBJECTS_DIR'] = f'{freesurfer_home}/subjects'
-    os.environ['PATH'] = f'{freesurfer_home}/bin:' + os.environ['PATH']
-
-
 def parse_args():
-    parser = argparse.ArgumentParser(description="plot subject aseg fig")
+    parser = argparse.ArgumentParser(description="plot subject norm to mni152 fig")
     parser.add_argument('--subject_id', help='输入的subjects id', required=True)
-    parser.add_argument('--subjects_dir', help='输入的subjects_dir文件', required=True)
-    parser.add_argument('--affine_mat', help='surf转换格式配准的affine', required=True)
+    parser.add_argument('--bold_preprocess_path', help='bold preprocess', required=True)
     parser.add_argument('--scene_file', help='画图所需要的scene文件', required=True)
-    parser.add_argument('--svg_outpath', help='输出的sav图片保存路径', required=True)
-    parser.add_argument('--freesurfer_home', help='freesurfer 的环境变量', default="/usr/local/freesurfer720",
-                        required=False)
+    parser.add_argument('--mni152_norm_png', help='模板MNI152的norm png图片', required=True)
+    parser.add_argument('--svg_outpath', help='输出的svg图片保存路径', required=True)
     args = parser.parse_args()
 
     return args
-
-
-def mgz2nii(mgz_file, nii_file):
-    cmd = f'mri_convert {mgz_file} {nii_file}'
-    os.system(cmd)
-
-
-def surf_nii2gii(nii_surf, gii_surf):
-    cmd = f'mris_convert {nii_surf} {gii_surf}'
-    os.system(cmd)
-
-
-def surface_apply_affine(giisurf_file, affine_mat):
-    cmd = f'wb_command -surface-apply-affine {giisurf_file} {affine_mat} {giisurf_file}'
-    os.system(cmd)
 
 
 def scene_plot(scene_file, savepath, length, width):
@@ -67,13 +48,14 @@ def scene_plot(scene_file, savepath, length, width):
     os.system(cmd)
 
 
-def write_single_svg(svg, png_back, view_height, view_width):
+def write_combine_svg(svg, png_back, png_fore, view_height, view_width):
     pngdata_back = encode_png(png_back)
+    pngdata_fore = encode_png(png_fore)
 
     with open(svg, 'w') as f:
         f.write(svg_img_head.format(view_height=view_height, view_width=view_width))
         f.write(svg_img_style)
-        f.write(svg_img_body_1.format(back_encode=pngdata_back))
+        f.write(svg_img_body_2.format(back_encode=pngdata_back, fore_encode=pngdata_fore))
         f.write(svg_img_tail)
 
 
@@ -85,65 +67,31 @@ def encode_png(png_img):
     return pngdata
 
 
-def rewrite_affine(surf_file, src_affine_mat):
-    _, _, header_info = nib.freesurfer.read_geometry(surf_file, read_metadata=True)
-    c_ras = header_info['cras']
-
-    affine_list = [[1, 0, 0, c_ras[0]], [0, 1, 0, c_ras[1]], [0, 0, 1, c_ras[2]], [0, 0, 0, 1]]
-
-    with open(src_affine_mat, 'w') as f:
-        for line in affine_list:
-            f.write(str(line)[1:-1].replace(',', '    ') + '\n')
-
-    f.close()
-
-
 if __name__ == '__main__':
     args = parse_args()
 
     subject_id = args.subject_id
-    subjects_dir = args.subjects_dir
-    affine_mat_atlas = args.affine_mat
+    bold_preprocess_dir = args.bold_preprocess_path
     scene_file = args.scene_file
+    mni152_norm_png = args.mni152_norm_png
     savepath_svg = args.svg_outpath
-    freesurfer_home = args.freesurfer_home
-    set_environ(freesurfer_home)
 
     subject_resultdir = Path(savepath_svg).parent
     if subject_resultdir.exists() is False:
         subject_resultdir.mkdir(parents=True, exist_ok=True)
-    subject_workdir = Path(subject_resultdir) / 'volsurf'
+    subject_workdir = Path(subject_resultdir) / 'norm2mni152'
     subject_workdir.mkdir(parents=True, exist_ok=True)
-    affine_mat = subject_workdir / 'affine.mat'
-    shutil.copyfile(affine_mat_atlas, affine_mat)
 
-    T1_mgz = Path(subjects_dir) / subject_id / 'mri' / 'T1.mgz'
-    T1_nii = subject_workdir / 'T1.nii.gz'
-    mgz2nii(T1_mgz, T1_nii)
-    lh_white = Path(subjects_dir) / subject_id / 'surf' / 'lh.white'
-    rh_white = Path(subjects_dir) / subject_id / 'surf' / 'rh.white'
-    lh_pial = Path(subjects_dir) / subject_id / 'surf' / 'lh.pial'
-    rh_pial = Path(subjects_dir) / subject_id / 'surf' / 'rh.pial'
+    norm_to_mni152nii = Path(
+        bold_preprocess_dir) / subject_id / 'anat' / f'{subject_id}_norm_space-MNI152_T1_2mm.nii.gz'
+    norm_to_mni152nii_tmp = subject_workdir / 'norm_to_mni152nii.nii.gz'
+    shutil.copyfile(norm_to_mni152nii, norm_to_mni152nii_tmp)
 
-    lh_white_gii = subject_workdir / 'lh.white.surf.gii'
-    rh_white_gii = subject_workdir / 'rh.white.surf.gii'
-    surf_nii2gii(lh_white, lh_white_gii)
-    surf_nii2gii(rh_white, rh_white_gii)
-    lh_pial_gii = subject_workdir / 'lh.pial.surf.gii'
-    rh_pial_gii = subject_workdir / 'rh.pial.surf.gii'
-    surf_nii2gii(lh_pial, lh_pial_gii)
-    surf_nii2gii(rh_pial, rh_pial_gii)
-    rewrite_affine(lh_white, affine_mat)
-    surface_apply_affine(lh_white_gii, affine_mat)
-    surface_apply_affine(rh_white_gii, affine_mat)
-    surface_apply_affine(lh_pial_gii, affine_mat)
-    surface_apply_affine(rh_pial_gii, affine_mat)
-
-    Vol_Surface_savepath = subject_workdir / 'Vol_Surface.png'
-    Vol_Surface_scene = subject_workdir / 'Vol_Surface.scene'
-    if Vol_Surface_scene.exists() is False:
-         shutil.copyfile(scene_file, Vol_Surface_scene)
-    scene_plot(Vol_Surface_scene, Vol_Surface_savepath, 2400, 1000)
-    Vol_Surface_savepath_svg = subject_resultdir / 'Vol_Surface.svg'
-    write_single_svg(Vol_Surface_savepath_svg, Vol_Surface_savepath, 2400, 1000)
+    NormtoMNI152_savepath = subject_workdir / 'NormtoMNI152.png'
+    NormtoMNI152_scene = subject_workdir / 'NormtoMNI152.scene'
+    if NormtoMNI152_scene.exists() is False:
+        shutil.copyfile(scene_file, NormtoMNI152_scene)
+    scene_plot(NormtoMNI152_scene, NormtoMNI152_savepath, 2400, 1000)
+    combine_svg_savepath = subject_resultdir / f'{subject_id}_desc-T1toMNI152_combine.svg'
+    write_combine_svg(combine_svg_savepath, mni152_norm_png, NormtoMNI152_savepath, 2400, 1000)
     shutil.rmtree(subject_workdir)
