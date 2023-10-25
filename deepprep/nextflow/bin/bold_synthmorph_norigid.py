f62406470362b9765f90474d61b0c8289027214b
lincong <lincong8722@qq.com>
2023-10-25 11:07:44 +0800

    CHG:add norm registration to native space and change the display file of qc_bold_norm_to_mni152

--- deepprep/nextflow/bin/bold_synthmorph_norigid.py
+++ deepprep/nextflow/bin/bold_synthmorph_norigid.py
@@ -5,11 +5,13 @@ import argparse
 import os
 
 
-def run_norigid_registration(subject_id, script, subj_anat_dir, T1_file, template, affine_trans, mp):
-    save_name = f'{subject_id}_T1_2mm_affine_synthmorph_space-MNI152_2mm'
-    moved = Path(subj_anat_dir) / f'{save_name}.nii.gz'
+def run_norigid_registration(subject_id, script, subj_func_dir, T1_file, norm_2mm, template, affine_trans, mp):
+    T1_save_name = f'{subject_id}_T1_2mm_affine_synthmorph_space-MNI152_2mm'
+    moved = Path(subj_func_dir) / f'{T1_save_name}.nii.gz'
 
-    cmd = f'python3 {script} -g -i {affine_trans} -o {moved} {T1_file} {template} -mp {mp}'
+    norm_save_name = f'{subject_id}_norm_2mm_affine_synthmorph_space-MNI152_2mm'
+    apply_output = Path(subj_func_dir) / f'{norm_save_name}.nii.gz'
+    cmd = f'python3 {script} -g -i {affine_trans} -o {moved} {T1_file} {template} -mp {mp} -a {norm_2mm} -ao {apply_output}'
     os.system(cmd)
 
 if __name__ == '__main__':
@@ -21,6 +23,7 @@ if __name__ == '__main__':
     parser.add_argument("--subject_id", required=True)
     parser.add_argument("--synth_script", required=True)
     parser.add_argument("--t1_native2mm", required=True)
+    parser.add_argument("--norm_native2mm", required=True)
     parser.add_argument("--affine_trans", required=True)
     parser.add_argument("--synth_model_path", required=True)
     parser.add_argument("--synth_template_path", required=True)
@@ -35,6 +38,7 @@ if __name__ == '__main__':
     # T1_2mm = subj_func_dir / f'{args.subject_id}_T1_2mm.nii.gz'
     # template = Path(args.synth_template_path) / 'MNI152_T1_2mm.nii.gz'
     T1_2mm = args.t1_native2mm
+    norm_2mm = args.norm_native2mm
     template = Path(args.synth_template_path) / 'MNI152_T1_2mm.nii.gz'
     affine_trans = subj_func_dir / f'{args.subject_id}_T1_2mm_affine_space-MNI152_2mm.xfm.txt'
-    run_norigid_registration(args.subject_id, args.synth_script, subj_func_dir, T1_2mm, template, affine_trans, args.synth_model_path)
+    run_norigid_registration(args.subject_id, args.synth_script, subj_func_dir, T1_2mm, norm_2mm, template, affine_trans, args.synth_model_path)
