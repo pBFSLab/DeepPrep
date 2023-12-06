104e59115df767605fe49edd71e91dff3e6e2b0f
Ning An <ninganme0317@gmail.com>
2023-12-06 17:16:19 +0800

    CHG: remove vxmmorph CHG: commond processes which not be used in paper CHG: change the RAM set CHG: change GPU lock count(setting 2)

--- deepprep/nextflow/bin/gpu_schedule_lock.py
+++ deepprep/nextflow/bin/gpu_schedule_lock.py
@@ -8,4 +8,9 @@ if __name__ == '__main__':
         print("set gpu lock")
         conn = StrictRedis()
         redis_lock.reset_all(conn)
-        lock = redis_lock.Lock(conn, 'nextflow-local-gpu')
+        lock1 = redis_lock.Lock(conn, 'nextflow-local-gpu-1')
+        lock2 = redis_lock.Lock(conn, 'nextflow-local-gpu-2')
+
+        conn.delete('lock_index')
+        for i in range(0, 10000):
+            conn.lpush('lock_index', i % 2)
