ae7cc604eeae30a9e0f999683dc0ae053591946a
NingAn <ninganme0317@gmail.com>
2023-12-06 17:16:19 +0800

    CHG: remove vxmmorph CHG: commond processes which not be used in paper CHG: change the RAM set CHG: change GPU lock count(setting 2)

--- deepprep/nextflow/bin/gpu_schedule_run.py
+++ deepprep/nextflow/bin/gpu_schedule_run.py
@@ -5,12 +5,27 @@ from redis_lock import StrictRedis
 
 
 if __name__ == '__main__':
+    print(sys.argv)
     if sys.argv[2].lower() == 'local':
         conn = StrictRedis()
-        with redis_lock.Lock(conn, 'nextflow-local-gpu'):
-            cmd = "python3 " + " ".join(sys.argv[3:])
-            print(cmd)
-            os.system(cmd)
+        if sys.argv[1] == 'double':
+            with redis_lock.Lock(conn, 'nextflow-local-gpu-1'), redis_lock.Lock(conn, 'nextflow-local-gpu-2'):
+                cmd = "python3 " + " ".join(sys.argv[3:])
+                print(cmd)
+                os.system(cmd)
+        else:
+            if 'lh' in sys.argv:
+                lock_name = 'nextflow-local-gpu-1'
+            elif 'rh' in sys.argv:
+                lock_name = 'nextflow-local-gpu-2'
+            else:
+                lock_i = int(conn.lpop('lock_index'))
+                lock_name = ['nextflow-local-gpu-1', 'nextflow-local-gpu-2'][lock_i]
+            print(f"lock_name: {lock_name}")
+            with redis_lock.Lock(conn, lock_name):
+                cmd = "python3 " + " ".join(sys.argv[3:])
+                print(cmd)
+                os.system(cmd)
     else:
         cmd = "python3 " + " ".join(sys.argv[3:])
         print(cmd)
