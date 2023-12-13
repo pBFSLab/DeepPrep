5b3f746d4d5dbf1adabfd70623896b984dab1533
Ning An <ninganme0317@gmail.com>
2023-12-13 11:29:33 +0800

    NEW: bold_skip_reorient.py 完成 文件名称的修改，增加 --reorient 参数 CHG: bold_get_bold_file_in_bids.py，--bids-dir 修改为 --bids_dir， --task 修改为 --task_type

--- deepprep/nextflow/bin/bold_skip_reorient.py
+++ deepprep/nextflow/bin/bold_skip_reorient.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import argparse
 import shutil
 
+
 def reorient_to_ras(input_path, output_path):
     img = nib.load(input_path)
     orig_ornt = nib.orientations.io_orientation(img.header.get_sform())
@@ -19,9 +20,9 @@ def reorient_to_ras(input_path, output_path):
         print(f"Successfully reorient {input_path} to RAS orientation and saved to {output_path}.")
 
 
-def cmd(subj_func_dir: Path, bold: Path, nskip_frame: int):
-    skip_bold = Path(subj_func_dir) / Path(bold).name.replace('.nii.gz', '_skip.nii.gz')
-    reorient_skip_bold = Path(subj_func_dir) / Path(bold).name.replace('.nii.gz', '_skip_reorient.nii.gz')
+def cmd(subj_func_dir: Path, bold: str, reorient: str, nskip_frame: int):
+    skip_bold = Path(subj_func_dir) / Path(bold).name.replace('_bold.nii.gz', '_desc-skip_bold.nii.gz')
+    reorient_skip_bold = Path(subj_func_dir) / Path(bold).name.replace('_bold.nii.gz', '_desc-reorient_bold.nii.gz')
 
     # skip 0 frame
     if nskip_frame > 0:
@@ -30,7 +31,8 @@ def cmd(subj_func_dir: Path, bold: Path, nskip_frame: int):
         skip_bold = bold
 
     # reorient
-    reorient_to_ras(skip_bold, reorient_skip_bold)
+    if reorient.upper() == 'TRUE':
+        reorient_to_ras(skip_bold, reorient_skip_bold)  # TODO 这里需要一个回传，告知是否进行了 reorient 操作, 并且返回Original orientation 和 Final orientation
 
 
 if __name__ == '__main__':
@@ -40,7 +42,8 @@ if __name__ == '__main__':
 
     parser.add_argument("--bold_preprocess_dir", required=True)
     parser.add_argument("--boldfile_path", required=True)
-    parser.add_argument("--nskip_frame", required=True)
+    parser.add_argument("--reorient", required=True)
+    parser.add_argument("--skip_frame", required=True)
     args = parser.parse_args()
 
 
@@ -53,5 +56,4 @@ if __name__ == '__main__':
     subj_func_dir = Path(preprocess_dir) / 'func'
     subj_func_dir.mkdir(parents=True, exist_ok=True)
 
-    cmd(subj_func_dir, bold_file, int(args.nskip_frame))
-
+    cmd(subj_func_dir, bold_file, args.reorient, int(args.skip_frame))
