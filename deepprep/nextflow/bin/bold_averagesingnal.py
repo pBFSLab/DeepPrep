500c485dbc7a192f4068b9a591d4403c202ef26f
NingAn <ninganme0317@gmail.com>
2023-12-17 15:07:05 +0800

    CHG: update filename for BIDS format

--- deepprep/nextflow/bin/bold_averagesingnal.py
+++ deepprep/nextflow/bin/bold_averagesingnal.py
@@ -47,7 +47,7 @@ class FramewiseDisplacementNode:
 
 
 class FMRISummaryNode:
-    def __call__(self, in_nifti, base_dir):
+    def __call__(self, in_nifti, in_segm, confounds_file, base_dir):
         confounds_list = [
             ("global_signal", None, "GS"),
             ("csf", None, "GSCSF"),
@@ -57,9 +57,9 @@ class FMRISummaryNode:
             ("rel_transform", None, "RHM")]
         FMRISummary_node = Node(FMRISummary(), f'FMRISummary_node')
         FMRISummary_node.inputs.in_nifti = in_nifti
-        FMRISummary_node.inputs.in_segm = Path(in_nifti).parent / Path(in_nifti).name.replace('bold.nii.gz', 'aseg.nii.gz')
+        FMRISummary_node.inputs.in_segm = in_segm
         FMRISummary_node.inputs.confounds_list = confounds_list
-        FMRISummary_node.inputs.confounds_file = Path(in_nifti).parent / Path(in_nifti).name.replace('bold.nii.gz', 'averagesingnal.tsv')
+        FMRISummary_node.inputs.confounds_file = confounds_file
         FMRISummary_node.base_dir = base_dir
 
         result = FMRISummary_node.run()
@@ -74,27 +74,28 @@ def calculate_mc(mcdat):
     return rel_transform.reshape(-1, 1)
 
 
-def AverageSingnal(bold_preprocess_dir, save_svg_dir, subject_id, bold_id, mc):
+def AverageSingnal(bold_preprocess_dir, save_svg_dir, subject_id, bold_id,
+                   mc, mcdat, aseg, brainmask, brainmask_bin, wm, csf):
     # ses = re.search(r'ses-(\w+\d+)', bold_id).group(0)
     base_dir = Path(bold_preprocess_dir) / 'tmp' / bold_id
     base_dir.mkdir(exist_ok=True, parents=True)
     bold = reshape_bold(mc)
-    roi_inf = {'global_signal': 'brainmask.bin', 'white_matter': 'wm', 'csf': 'csf'}
+    roi_inf = {'global_signal': brainmask_bin, 'white_matter': wm, 'csf': csf}
     results = []
     for key in roi_inf.keys():
-        annot_file = Path(bold_preprocess_dir) / 'func' / mc.name.replace('bold.nii.gz', f'{roi_inf[key]}.nii.gz')
+        annot_file = roi_inf[key]
         parc_annot = reshape_annot(annot_file)
         roi_mean = np.mean(bold[np.where(parc_annot == 1.0)[0]], axis=0)
         results.append(np.expand_dims(roi_mean, 1))
     columns = list(roi_inf.keys())
-    bold_mask = Path(bold_preprocess_dir) / 'func' / mc.name.replace('bold.nii.gz', 'brainmask.nii.gz')
+    bold_mask = brainmask
     compute_dvars = ComputeDVARSNode()
     std_dvars_path = compute_dvars(mc, bold_mask, base_dir)
     std_dvars = pd.read_csv(std_dvars_path, header=None).values
     std_dvars = np.insert(std_dvars, 0, np.array([np.nan]), axis=0)
     results.append(std_dvars)
     columns.append('std_dvars')
-    mcdat = Path(bold_preprocess_dir) / 'func' / f'{bold_id}_from-stc_to-mc_xfm.mcdat'
+    mcdat = mcdat
     framewisedisplacement = FramewiseDisplacementNode()
     fd_path = framewisedisplacement(mcdat, base_dir)
     fd = pd.read_csv(fd_path, sep='\t', encoding='utf-8').values
@@ -106,11 +107,11 @@ def AverageSingnal(bold_preprocess_dir, save_svg_dir, subject_id, bold_id, mc):
     columns.append('rel_transform')
     data = np.concatenate(results, axis=1).astype(np.float32)
     data_df = pd.DataFrame(data=data, columns=columns)
-    csv_file = Path(bold_preprocess_dir) / 'func' / mc.name.replace('bold.nii.gz', 'averagesingnal.tsv')
+    csv_file = Path(bold_preprocess_dir) / 'func' / mc.name.replace('bold.nii.gz', 'desc-averagesingnal_timeseries.tsv')
     data_df.to_csv(csv_file, sep="\t")
 
     summary = FMRISummaryNode()
-    summary_path = summary(mc_file, base_dir)
+    summary_path = summary(mc_file, aseg, csv_file, base_dir)
     carpet_path = save_svg_dir / f'{bold_id}_desc-carpet_bold.svg'
     cmd = f'cp {summary_path} {carpet_path}'
     os.system(cmd)
@@ -126,7 +127,11 @@ if __name__ == '__main__':
     parser.add_argument("--subject_id", required=True)
     parser.add_argument("--mc", required=True)
     parser.add_argument("--mcdat", required=True)
+    parser.add_argument("--anat_aseg", required=True)
     parser.add_argument("--anat_brainmask", required=True)
+    parser.add_argument("--anat_brainmaskbin", required=True)
+    parser.add_argument("--anat_wm", required=True)
+    parser.add_argument("--anat_csf", required=True)
     parser.add_argument("--bold_id", required=True)
     parser.add_argument("--save_svg_dir", required=True)
     args = parser.parse_args()
@@ -137,8 +142,16 @@ if __name__ == '__main__':
     subj_func_dir = Path(preprocess_dir) / 'func'
     subj_func_dir.mkdir(parents=True, exist_ok=True)
     # bold_preprocess_path = Path(cur_path) / Path(args.bold_preprocess_dir).name
-    mc_file = subj_func_dir / f'{args.bold_id}_desc-mc_bold.nii.gz'
+    # mc_file = subj_func_dir / f'{args.bold_id}_space-mc_bold.nii.gz'
+    mc_file = subj_func_dir / os.path.basename(args.mc)
+    mcdat_file = subj_func_dir / os.path.basename(args.mcdat)
+    aseg_file = subj_func_dir / os.path.basename(args.anat_aseg)
+    brainmask_file = subj_func_dir / os.path.basename(args.anat_brainmask)
+    brainmaskbin_file = subj_func_dir / os.path.basename(args.anat_brainmaskbin)
+    wm_file = subj_func_dir / os.path.basename(args.anat_wm)
+    csf_file = subj_func_dir / os.path.basename(args.anat_csf)
     svg_path = Path(cur_path) / str(args.save_svg_dir) / args.subject_id / 'figures'
     svg_path.mkdir(parents=True, exist_ok=True)
-    AverageSingnal(preprocess_dir, svg_path, args.subject_id, args.bold_id, mc_file)
+    AverageSingnal(preprocess_dir, svg_path, args.subject_id, args.bold_id,
+                   mc_file, mcdat_file, aseg_file, brainmask_file, brainmaskbin_file, wm_file, csf_file)
 
