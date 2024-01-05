6751d250037f4e558c6e1431bab7e51119b3d705
NingAn <ninganme0317@gmail.com>
2024-01-05 11:02:37 +0800

    NEW: add lincense in the sdc py FIX: add assert in the sdc py

--- deepprep/nextflow/bin/bold_resampling.py
+++ deepprep/nextflow/bin/bold_resampling.py
@@ -1,12 +1,26 @@
 """Interfaces for resampling images in a single shot"""
+#
+# STATEMENT OF CHANGES: This file is derived from sources licensed under the Apache-2.0 terms,
+# and this file has been changed.
+#
+# Copyright 2023 The NiPreps Developers <nipreps@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import asyncio
 import os
 from functools import partial
 
 import nibabel as nb
-import nitransforms as nt
-import numpy as np
 from nipype.interfaces.base import (
     File,
     InputMultiObject,
