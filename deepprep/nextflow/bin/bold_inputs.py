0b51ab56f634d8bfa4459f3af822ed9102c0903f
Ireneyou33 <ireneyou33@gmail.com>
2024-01-11 11:18:09 +0800

    CHG: clean version

--- deepprep/nextflow/bin/bold_inputs.py
+++ deepprep/nextflow/bin/bold_inputs.py
@@ -7,16 +7,6 @@ import numpy as np
 import sh
 
 
-def set_envrion():
-    # FreeSurfer recon-all env
-    os.environ['FREESURFER_HOME'] = "/usr/local/freesurfer720"
-    os.environ['FREESURFER'] = "/usr/local/freesurfer720"
-    os.environ['SUBJECTS_DIR'] = "/home/youjia/Downloads"
-    os.environ['PATH'] = ('/usr/local/freesurfer720/bin:'
-                          + '/home/youjia/abin:'
-                          + '/usr/local/c3d/bin:'
-                          + os.environ['PATH'])
-
 def get_wm_dseg_file(aparc_aseg_mgz, wm_dseg_nii):
     data = ants.image_read(aparc_aseg_mgz)
     data_origin = data.origin[:3]
@@ -42,14 +32,11 @@ def cmd(t1_mgz, mask_mgz, aparc_aseg_mgz, t1_nii, mask_nii, wm_dseg_nii, fsnativ
     os.system(f"mri_convert {t1_mgz} {t1_nii}")
     os.system(f"mri_convert {mask_mgz} {mask_nii}")
 
-    # sh.mri_convert(t1_mgz, t1_nii)
-    # sh.mri_convert(mask_mgz, mask_nii)
     get_wm_dseg_file(aparc_aseg_mgz, wm_dseg_nii)
     get_fsnative2T1w_xfm(fsnative2T1w_xfm)
 
 
 if __name__ == '__main__':
-    # set_envrion()
 
     parser = argparse.ArgumentParser(
         description="DeepPrep: Prepare input data for BOLD process"
@@ -64,8 +51,4 @@ if __name__ == '__main__':
     parser.add_argument("--fsnative2T1w_xfm", required=True)
     args = parser.parse_args()
 
-
-    print("START!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    print(args.t1_mgz)
-    print(args.mask_mgz)
     cmd(args.t1_mgz, args.mask_mgz, args.aparc_aseg_mgz, args.t1_nii, args.mask_nii, args.wm_dseg_nii, args.fsnative2T1w_xfm)
