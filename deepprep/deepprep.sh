5bbc18a44ae10591bd42ef7d96d59f782469549f
NingAn <ninganme0317@gmail.com>
2024-01-29 15:23:17 +0800

    CHG: Optimize the number of input parameters for the DeepPrep run command.

--- deepprep/deepprep.sh
+++ deepprep/deepprep.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
+echo "$@"
+
 if pgrep redis-server > /dev/null
 then
-  echo "Redis is already running."
+  echo "INFO: Redis is already running."
 else
-  echo "Starting Redis..."
+  echo "INFO: Starting Redis..."
   nohup redis-server > /dev/null 2>&1 &
-  echo "Redis is running."
+  echo "INFO: Redis is running."
 fi
 
 source /usr/local/freesurfer/SetUpFreeSurfer.sh
 
 # 定义目录路径
-work_dir="/nextflow/workDir"
+if [ "$1" != "--bids_dir" ]; then
+    echo "$1 != --bids_dir"
+    exit 1
+fi
+if [ "$3" != "--output_dir" ]; then
+    echo "$3 != --output_dir"
+    exit 1
+fi
+bids_dir=$2
+output_dir=$4
+nextflow_work_dir="${output_dir}/WorkDir/nextflow"  # output_dir/WorkDir/nextflow
+qc_dir="${output_dir}/QC"  # output_dir/WorkDir/nextflow
 
 # 判断目录是否存在
-if [ ! -d "$work_dir" ]; then
+if [ ! -d "$nextflow_work_dir" ]; then
   # 目录不存在，进行创建
-  mkdir -p "$work_dir"
-  echo "create workDir successfully"
+  mkdir -p "$nextflow_work_dir"
+  echo "INFO: create  nextflow WorkDir: $nextflow_work_dir"
 else
-  echo "workDir already exist"
+  echo "INFO: existed nextflow WorkDir: $nextflow_work_dir"
 fi
-echo "$work_dir"
 
-cd "$work_dir"
-echo "$@"
-nextflow "$@"
+cd "$nextflow_work_dir" && \
+nextflow run /opt/DeepPrep/deepprep/nextflow/deepprep.nf \
+-c /opt/DeepPrep/deepprep/nextflow/nextflow.docker.local.config \
+-w "${nextflow_work_dir}" \
+-with-report "${qc_dir}/report.html" \
+-with-timeline "${qc_dir}/timeline.html" \
+"$@"
