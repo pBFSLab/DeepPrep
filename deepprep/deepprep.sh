2b6a3315cb320fe4c587c9531b990a32c4f061d4
NingAn <ninganme0317@gmail.com>
2024-01-30 15:37:01 +0800

    FIX: '{' Error
    NHG: --subjects to --participant_label

--- deepprep/deepprep.sh
+++ deepprep/deepprep.sh
@@ -13,12 +13,12 @@ ignore_error=""
 debug=""
 
 help="DeepPrep args:
-deepprep-docker [bids_dir] [output_dir] [{participant}] [--bold_task_type task_id]
-                [--fs_license_file PATH]
-                [--subjects_dir subjects_dir] [--executor {local cluster}]
+deepprep-docker [bids_dir] [output_dir] [{participant}] [--bold_task_type TASK_LABEL]
+                [--fs_license_file PATH] [--participant-label PARTICIPANT_LABEL [PARTICIPANT_LABEL ...]]
+                [--subjects_dir PATH] [--executor {local cluster}]
                 [--anat_only] [--bold_only] [--bold_sdc] [--bold_confounds]
-                [--bold_surface_spaces [fsnative fsaverage fsaverage6 ...]]
-                [--bold_template_space MNI152NLin6Asym] [--bold_template_res {02 03...}]
+                [--bold_surface_spaces '[fsnative fsaverage fsaverage6 ...]']
+                [--bold_template_space {MNI152NLin6Asym MNI152NLin2009cAsym}] [--bold_template_res {02 03...}]
                 [--device {auto {0 1 2...} cpu}] [--gpu_compute_capability {8.6}]
                 [--cpus 10] [--memory 5]
                 [--freesurfer_home PATH] [--deepprep_home PATH] [--templateflow_home PATH]
